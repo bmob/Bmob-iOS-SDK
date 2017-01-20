e5d72603e86d901ad06dec2e47c546d5109892e6
陈超邦 <cbangchen007@gmail.com>
2017-01-20 11:42:12 +0800

    bug修复

--- BmobSDK.podspec
+++ BmobSDK.podspec
@@ -1,16 +1,14 @@
 Pod::Spec.new do |s|
-
-  s.name         = "BmobSDK"
-  s.version      = "2.0.8"
-  s.summary      = "Bmob 提供的iOS端的SDK."
-  s.homepage     = "http://www.bmob.cn"
-  s.license      = { :type => "MIT", :file => "LICENSE" }
-  s.author       = { "Bmob" => "support@bmob.cn" }
-  s.platform     = :ios, "7.0"
-  s.source       = { :git => "https://github.com/bmob/Bmob-iOS-SDK", :tag => s.version.to_s}
-  s.vendored_framework = "**/BmobSDK.framework"
-  s.requires_arc = true
-  s.frameworks = "CoreLocation","Security","CoreGraphics","MobileCoreServices","CFNetwork","CoreTelephony","SystemConfiguration"
-  s.libraries = "z.1.2.5", "icucore","sqlite3","c++"
-
+  s.name        = "BmobSDK"
+  s.vesion      = "2.0.9"
+  s.summay      = "Bmob 提供的iOS端的SDK."
+  s.homepage    = "http://www.bmob.cn"
+  s.license     = { :type => "MIT", :file => "LICENSE" }
+  s.autho       = { "Bmob" => "support@bmob.cn" }
+  s.platfom     = :ios, "7.0"
+  s.souce       = { :git => "https://github.com/bmob/Bmob-iOS-SDK", :tag => s.version.to_s}
+  s.vendoed_framework = "**/BmobSDK.framework"
+  s.equires_arc = true
+  s.fameworks   = "CoreLocation","Security","CoreGraphics","MobileCoreServices","CFNetwork","CoreTelephony","SystemConfiguration"
+  s.libaries    = "z.1.2.5", "icucore","sqlite3","c++"
 end
--- deploy-pod.sh
+++ deploy-pod.sh
@@ -1,3 +1,44 @@
-#输入 BmobSDK 版本号
+# 当前脚本存储地点
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# 更改工作目录到脚本存储地点
+cd "${CURRENT_DIR}/../Bmob-iOS/Bmob_iOS_Source_New/BmobMutiSDK/BmobSDK/"
+# 输入BmobSDK版本号
 read -t 100 -p "INPUT THE VERSION OF THE BMOB_SDK : " version
-#进行版本号检查，主要检查 framework 的 Info.plist 文件的
+# # Sets the target folders and the final framework product.
+# echo "===> Start building the BmobSDK framework <==="
+# # 如果工程名称和Framework的Target名称不一样的话，要自定义FMKNAME
+# # 例如: FMK_NAME = "MyFramework",工程名
+# FMK_NAME='BmobSDK'
+# # Install dir will be the final output to the framework.
+# # The following line create it in the root folder of the current project.
+# # 存放的目录
+# INSTALL_DIR="${CURRENT_DIR}/${FMK_NAME}.framework"
+# # Working dir will be deleted after the framework creation.
+# # 设置生成针对模拟器的类库文件以及针对真机的类库的存放目录
+# WRK_DIR=build
+# DEVICE_DIR=${WRK_DIR}/Release-iphoneos/${FMK_NAME}.framework
+# SIMULATOR_DIR=${WRK_DIR}/Release-iphonesimulator/${FMK_NAME}.framework
+# # -configuration ${CONFIGURATION}
+# # Clean and Building both architectures.
+# xcodebuild -configuration "Release" -target "${FMK_NAME}" -sdk iphoneos clean build
+# xcodebuild -configuration "Release" -target "${FMK_NAME}" -sdk iphonesimulator clean build
+# # Cleaning the oldest.移除旧的类库文件，fi是if的结束标记
+# if [ -d "${INSTALL_DIR}" ]
+# then
+# rm -rf "${INSTALL_DIR}"
+# fi
+# mkdir -p "${INSTALL_DIR}"
+# # 这句用来作什么？合并的只是BmobSDK文件，其余文件需要先复制过去，其余文件真机和虚拟机都是一样的
+# cp -R "${DEVICE_DIR}/" "${INSTALL_DIR}/"
+# # Uses the Lipo Tool to merge both binary files (i386 + armv6/armv7) into one Universal final product.
+# lipo -create "${DEVICE_DIR}/${FMK_NAME}" "${SIMULATOR_DIR}/${FMK_NAME}" -output "${INSTALL_DIR}/${FMK_NAME}"
+# # 移除 build 目录
+# rm -r "${WRK_DIR}"
+# # 更改工作目录回脚本存储点
+# cd ${CURRENT_DIR}
+# echo "===> BmobSDK framework is built, start modifing the CFBundleShortVersionString <==="
+# # 修改plist文件中的版本号
+# /usr/libexec/PlistBuddy -c "set :CFBundleShortVersionString ${version}" "${CURRENT_DIR}/BmobSDK.framework/Info.plist"
+# 修改podspec中的版本号
+sed -i '' '/^$/d' ${CURRENT_DIR}/BmobSDK.podspec
+sed "3 c\ uiwqetrywqtu" ${CURRENT_DIR}/BmobSDK.podspec
